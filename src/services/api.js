4f95fd3e7aa55f2ff2cbd60de3288ece5331e8f6
baraka0123 <barakaarnauld@gmail.com>
2026-01-31 20:24:09 -0800

    connect frontend to render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://chemovigi-backend.onrender.com/api",
+  baseURL: "https://chemovigi-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
